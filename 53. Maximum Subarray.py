3f9a69e4b2c69dbeea4b593125b373a3c663505d
jlyang1990 <jlyang@ucdavis.edu>
2016-11-15 15:05:19 -0800

    Add code file

---
+++ 53. Maximum Subarray.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # maxSubArray = max(maxSubArray_endwithindex0, maxSubArray_endwithindex1, ..., maxSubArray_endwithindex(n-1))
+        maxsum = [None] * len(nums)
+        maxsum[0] = nums[0]
+        for i in range(1, len(nums)):
+            maxsum[i] = max(maxsum[i-1], 0) + nums[i]
+        return max(maxsum)
