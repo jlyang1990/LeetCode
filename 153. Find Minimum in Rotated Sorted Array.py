799c093d5073fbe8799a82ea08a2a864b338d964
jlyang1990 <jlyang@ucdavis.edu>
2016-11-15 16:16:36 -0800

    Add code file

---
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        first = 0
+        last = len(nums) - 1
+        while first < last:
+            mid = (first+last)/2
+            if nums[mid] < nums[last]:
+                last = mid
+            else:
+                first = mid + 1
+        return nums[first]
