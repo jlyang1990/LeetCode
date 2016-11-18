6c42f3366856ce2aa7779018e1950063e5e6b60c
jlyang1990 <jlyang@ucdavis.edu>
2016-11-18 00:54:11 -0800

    Add code file

---
+++ 215. Kth Largest Element in an Array.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # quick selection
+        # average time complexity: O(n)+O(n/2)+O(n/4)+...=O(n)
+        def partition(nums, first, last):
+            left = first+1
+            right = last
+            while left <= right:
+                while left <= right and nums[left] <= nums[first]:
+                    left += 1
+                while left <= right and nums[right] >= nums[first]:
+                    right -= 1
+                if left <= right:
+                    nums[left], nums[right] = nums[right], nums[left]
+            nums[first], nums[right] = nums[right], nums[first]
+            return right
+            
+        def findKthLargestHelper(nums, k, first, last):
+            loc = partition(nums, first, last)
+            if last - loc + 1 == k:
+                return nums[loc]
+            elif last - loc + 1 > k:
+                return findKthLargestHelper(nums, k, loc+1, last)
+            else:
+                return findKthLargestHelper(nums, k-(last-loc+1), 0, loc-1)
+        
+        return findKthLargestHelper(nums, k, 0, len(nums)-1)
